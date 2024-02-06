e331cd9c87d68d7fc2189b0d0e8d075116481b93
Sushil Bhandari <zoroat3@gmail.com>
2024-02-06 12:45:46 +0000

    Change to MiUI style

--- cm20314/Components/navbar.tsx
+++ cm20314/Components/navbar.tsx
@@ -1,5 +1,54 @@
 import StarIcon from '@mui/icons-material/Star';
-export default function Navbar () {
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+
+export default function Navbar() {
+  return (
+    <Box sx={{ backgroundColor: '#0B254A', color: '#C5C5C5', padding: '10px', width: '100%' }}>
+      <Grid container justifyContent="space-between">
+        <Grid item>
+          <ul style={{ listStyleType: 'none', margin: '0', padding: '0' }}>
+            <li style={{ display: 'inline' }}>
+              <a href="#" style={{ textDecoration: 'none', padding: '10px', color: 'grey' }}>
+                Home
+              </a>
+            </li>
+            <li style={{ display: 'inline' }}>
+              <a href="#" style={{ textDecoration: 'none', padding: '10px', color: 'grey' }}>
+                History
+              </a>
+            </li>
+            <li style={{ display: 'inline' }}>
+              <a href="#" style={{ textDecoration: 'none', padding: '10px', color: 'grey' }}>
+                Account
+              </a>
+            </li>
+          </ul>
+        </Grid>
+        <Grid item>
+          <Box
+            sx={{
+              display: 'flex',
+              justifyContent: 'space-between',
+              backgroundColor: 'lightblue',
+              padding: '5px 10px',
+              margin: '0',
+              width: '8rem',
+              color: 'darkgray',
+            }}
+          >
+            <span>John Doe</span>
+            <StarIcon />
+          </Box>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+}
+
+
+
+/*export default function Navbar () {
     return (
         <nav 
         style = {{
@@ -72,4 +121,4 @@ export default function Navbar () {
 
         </nav>
     )
-  }
+  }*/
