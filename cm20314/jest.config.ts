fefea592a7eaeb06d78a715c9c73303da8078075
mbm60-max <mbm60@bath.ac.uk>
2024-02-09 15:58:46 +0000

    added coverage metric

--- cm20314/jest.config.ts
+++ cm20314/jest.config.ts
@@ -4,6 +4,17 @@ import type { Config } from '@jest/types';
 const config: Config.InitialOptions = {
   // Other Jest configurations...
   testEnvironment: 'jsdom',
+  collectCoverage: true,
+  coverageReporters: ['json', 'lcov', 'text', 'clover'],
+  coverageDirectory: 'coverage',
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80
+    }
+  }
 };
 
 export default config;
