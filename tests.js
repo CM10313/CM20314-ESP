ee016bd5225f0546010253b969486f472ac1431e
Sushil <34659821+sush-il@users.noreply.github.com>
2025-05-08 12:52:45 +0100

    Create tests.js

---
+++ tests.js
@@ -0,0 +1,4 @@
+// test-secrets.js
+const awsKey = "AKIA1234567890EXAMPLE";
+const slackWebhook = "https://hooks.slack.com/services/T00000000/B00000000/XXXXXXXXXXXXXXXXXXXXXXXX";
+const githubToken = "ghp_1234567890abcdefghijklmnopqrstuvwxyzABCD";
